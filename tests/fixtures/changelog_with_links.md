e493ad4607e3792f909cfe2f5e9f5a61c8555c0c
Alessandro Dalfovo <dalfovo.alessandro@gmail.com>
2022-06-24 17:12:40 +0200

    Add test for changelog with links

---
+++ tests/fixtures/changelog_with_links.md
@@ -0,0 +1,16 @@
+# Changelog
+
+## [Unreleased]
+
+## [0.2.0] - 2022-01-02
+
+Second release 🚀🚀
+
+## [0.1.0] - 2022-01-01
+
+First release 🚀
+
+
+[Unreleased]: https://github.com/olivierlacan/keep-a-changelog/compare/v0.2.0...HEAD
+[0.2.0]: https://github.com/olivierlacan/keep-a-changelog/compare/v0.1.0...v0.2.0
+[0.1.0]: https://github.com/olivierlacan/keep-a-changelog/releases/tags/v1.0.0
--- tests/test_python_changelog.py
+++ tests/test_python_changelog.py
@@ -16,6 +16,7 @@ def test_changelog_extraction():
     assert c.get_changelog_of_latest_tag(get_file_path("changelog_with_multiple_versions.md")) == ('0.2.0', '\nSecond release 🚀🚀\n\n')
     assert c.get_changelog_of_latest_tag(get_file_path("empty_changelog.md")) == (None, '')
     assert c.get_changelog_of_latest_tag(get_file_path("changelog_with_one_version.md")) == ('0.1.0', '\nFirst release 🚀\n')
+    assert c.get_changelog_of_latest_tag(get_file_path("changelog_with_links.md")) == ('0.2.0', '\nSecond release 🚀🚀\n\n')
 
 def get_file_path(filename):
     return Path("tests/fixtures/"+filename)
