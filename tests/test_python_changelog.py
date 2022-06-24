1eee1abec1d0a25630a0516f855a1c250776aae1
Cristiano Piemontese <cristiano.piemontese@prima.it>
2022-06-24 15:09:45 +0200

    refactor tests

--- tests/test_python_changelog.py
+++ tests/test_python_changelog.py
@@ -6,7 +6,7 @@ def test_version():
     assert __version__ == '0.1.0'
 
 def test_find_latest_tag():
-    assert c.find_latest_tag("Non existing changelog.md") == None
+    assert c.find_latest_tag(Path("Non existing changelog.md")) == None
     assert c.find_latest_tag(Path("tests/fixtures/empty_changelog.md")) == None
     assert c.find_latest_tag(Path("tests/fixtures/changelog_with_unreleased_and_version.md")) == "0.1.0"
     assert c.find_latest_tag(Path("tests/fixtures/changelog_with_one_version.md")) == "0.1.0"
